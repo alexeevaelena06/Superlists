a4c5f7691694ab78e4b43a6f23bd4a57cd8e2bb0
alexeevaelena06 <alexeevaelena06@gmail.com>
2020-02-29 13:26:10 +0300

    add module test: test_root_url_resolves_to_home_page_view

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,12 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
-    """Тест на токсичность"""
-    def test_bad_math(self):
-        """тест: неправильные математические расчёты"""
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    """Тест домашней страницы"""
+
+    def test_root_url_resolves_to_home_page_view(self):
+        """тест: корневой url преобразуется в представление домашней страницы"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
