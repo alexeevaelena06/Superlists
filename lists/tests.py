d9742840774a36074bc2afaddb1ca549122b98b0
alexeevaelena06 <alexeevaelena06@gmail.com>
2020-03-02 22:08:00 +0300

    рефакторинг tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -7,19 +7,9 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    """Тест домашней страницы"""
+    """тест домашней страницы"""
 
-    def test_root_url_resolves_to_home_page_view(self):
-        """тест: корневой url преобразуется в представление домашней страницы"""
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        """тест: домашняя страница возвращает правильный html"""
+    def test_uses_home_template(self):
+        """тест: используется домашний шаблон"""
         response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
         self.assertTemplateUsed(response, 'home.html')
