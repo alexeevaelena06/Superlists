a8014a9c47e65135fc9ac2432b10b7e51e1717d1
alexeevaelena06 <alexeevaelena06@gmail.com>
2020-03-09 21:44:07 +0300

    update tests.py and views.py

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from lists.models import Item
 from django.http import HttpResponse
 
@@ -7,10 +7,7 @@ from django.http import HttpResponse
 def home_page(request):
     """Домашняя страница"""
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text,
-    })
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    return render(request, 'home.html')
